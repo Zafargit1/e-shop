95925660f93b4e2f5febbea95ebb66c8cfc57ae1
Zafar <zafonline23@gmail.com>
2026-05-06 19:44:37 +0300

    Error fix

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -60,7 +60,7 @@ const LoginSignup = () => {
             <div className="loginsignup-container">
                 <h1>{state}</h1>
                 <div className="loginsignup-fields">
-                    {state === "Sign Up" ? <input name="username" value={formData.username} onChange={changeHandler} type="text" placeholder='Your Name' onChange={changeHandler} /> : <></>}
+                    {state === "Sign Up" ? <input name="username" value={formData.username} onChange={changeHandler} type="text" placeholder='Your Name'/> : <></>}
                     <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder='Your Email address' />
                     <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder='Password' />
                 </div>
